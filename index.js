e4a6fce723aef2c73c5d4c92daee35d78c9e68a6
Ronsen <ronsen@startrek.or.id>
2025-03-10 16:05:30 +0700

    update: port

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function getRandomColor() {
 }
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
